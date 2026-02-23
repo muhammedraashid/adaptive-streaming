afdc8e82a709704179c1788ba4484ca36e294761
rashid <muhammedrashidn881@gmail.com>
2026-02-23 18:25:59 +0530

    feat: server HLS playlists and segments  with CORS enabled for browser playback

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,11 @@
 
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.routes import upload
+from app.core.config import MEDIA_ROOT
 from app.db.session import Base, engine
 
 
@@ -10,3 +14,13 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Adaptive Streaming API")
 
 app.include_router(upload.router)
+
+app.mount("/media", StaticFiles(directory=MEDIA_ROOT), name='media')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
